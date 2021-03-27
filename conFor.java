7cca47018e235b79e61af3022c38b881f2e6500c
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-27 14:09:29 +0100

    Create conFor.java

---
+++ conFor.java
@@ -0,0 +1,7 @@
+ public class conFor {
+    public static void main(String[] args) {
+        System.out.println("Numeros del 1 al 100 con bucle For: ");
+        for(int i = 1; i<=100;i++)
+            System.out.println(i);
+    }
+}
